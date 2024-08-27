01de8792d7ff05e732297af6386ebe33c14a2c08
Potineedi <hpotineedi@contractor.ea.com>
2024-08-27 20:03:28 +0530

    27/08 Changes

--- Frontend/tcloth/src/Components/WomenCollectionsCarousel/Carousel.jsx
+++ Frontend/tcloth/src/Components/WomenCollectionsCarousel/Carousel.jsx
@@ -8,6 +8,8 @@ import jewImage1 from '../../assets/MidSectionImages/JeweleryImages/JeweleryOne.
 import jewImage2 from '../../assets/MidSectionImages/JeweleryImages/JeweleryTwo.png';
 import MenWearOne from '../../assets/MidSectionImages/MensWear/MenWearOne.png';
 import MenWearTwo from '../../assets/MidSectionImages/MensWear/MenWearTwo.png';
+import Slide4Img from '../../assets/MidSectionImages/4thSlide/Image1.png';
+import Slide5Img from '../../assets/MidSectionImages/5thSlide/Image1.png';
 
 
 const Carousel = () => {
@@ -26,6 +28,10 @@ const Carousel = () => {
 
   return (
     <div className='container-fluid CollectionMid'>
+
+
+
+      {/*1st Slide*/}
         <div className='womenCollectionSection'> 
             <h5><a href="" style={{color: "black"}}>Shop By Category</a></h5>
             <span style={{color: "#693621"}}><strong style={{fontSize: "50px"}}>READY</strong>  to wear</span>
@@ -37,15 +43,44 @@ const Carousel = () => {
             </div>
         </div>
 
+
+        {/*2nd Slide*/}
         <div className='JewelerySection'>
           {jewImg ? (<img src={jewImage1} onClick={handleJewClick} alt='Jew1' className='JeweImage'/>) : (<img src={jewImage2} onClick={handleJewClick} alt='Jew2' className='JeweImage'/>)}
         </div>
 
+
+        {/*3rd Slide*/}
         <div className='MenWearSection'>
           {menWearImg ? (<img src={MenWearOne} onClick={handleMenWear} alt='MewnW1' className='menWearImg'></img>) : (<img src={MenWearTwo} onClick={handleMenWear} alt='MenW2' className='menWearImg'/>)}
-          <button type="button" className="btn menWearButton">Shop Now</button>
-          
+          <button type="button" className="btn menWearButton"><p className='menswearBtnText'>Shop Now</p></button>         
         </div>
+
+        
+        {/*4th Slide*/}
+        <div className='Slide4'>
+            <img src={Slide4Img} alt="Slide4Img" className='slide4Image'/>
+            <button type="button" className="btn slide4btn"><p className='slide4BtnText'>Shop Now</p></button>  
+        </div>
+
+        {/*4th Slide*/}
+        <div className="container-fluid Slide5th">
+            <div style={{color: "#693621"}} className="Slide5Content">
+                <h1 className="Slide5Heading">THE WEDDING<br /> EDIT</h1>
+                <br />
+                <p style={{ fontWeight: "500", opacity: "0.7"}} className="Slide5Text">
+                    Timeless lehengas handcrafted in <br />
+                    Pichhwari,Bandhani and Benarasi
+                </p> <br />     
+
+                <button type="button" className="btn btn-lg Slide5ShopNowBtn" style={{backgroundColor: "#693621", color:"white"}}>Shop Now</button>
+            </div> <br />
+
+            <div>
+                <img src={Slide5Img} alt="" className="Slide5Img"/>
+            </div>
+         </div>
+
     </div>
   )
 }
